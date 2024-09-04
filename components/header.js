b4c6c148046e00ff5c17c8674c90954ec56baa30
gtz.jesus <gtz.jesus@outlook.com>
2024-09-04 12:12:06 -0600

    Optimization with images lectures

--- components/header.js
+++ components/header.js
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import Image from 'next/image'
+import Image from 'next/image';
 
 import logo from '@/assets/logo.png';
 
@@ -7,7 +7,14 @@ export default function Header() {
   return (
     <header id="main-header">
       <Link href="/">
-        <Image src={logo.src} alt="Mobile phone with posts feed on it" />
+        <Image
+          src={logo}
+          width={100}
+          height={100}
+          priority
+          // sizes="10vw"
+          alt="Mobile phone with posts feed on it"
+        />
       </Link>
       <nav>
         <ul>
@@ -15,7 +22,9 @@ export default function Header() {
             <Link href="/feed">Feed</Link>
           </li>
           <li>
-            <Link className='cta-link' href="/new-post">New Post</Link>
+            <Link className="cta-link" href="/new-post">
+              New Post
+            </Link>
           </li>
         </ul>
       </nav>
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // Configuartion Code to Allow loading images
+  images: {
+    remotePatterns: [{ hostname: 'res.cloudinary.com' }],
+  },
+};
 
 export default nextConfig;
